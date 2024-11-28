7072ff70cc0665692865e6e48ea44f8895890014
Aleksandr Chubkov <aleksandrchubkov@stg5.relicts.renins.com>
2024-11-28 11:22:03 +0300

    init

---
+++ otus/otusApp.swift
@@ -0,0 +1,17 @@
+//
+//  otusApp.swift
+//  otus
+//
+//  Created by Aleksandr Chubkov on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct otusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
